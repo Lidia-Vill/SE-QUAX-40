a27163f5674d8190e897a28485decd9f8d1dfc97
Lidia-Vill <villanueva.lidia@ucdconnect.ie>
2026-02-16 20:02:25 +0000

    Changed APP Name

--- src/main/java/com/example/sequax40/QuaxController.java
+++ src/main/java/com/example/sequax40/QuaxController.java
@@ -3,7 +3,7 @@ package com.example.sequax40;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class QuaxController {
     @FXML
     private Label welcomeText;
 
