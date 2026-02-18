6b3d63bed3adecefc48258323836227db3a4c601
Gwyneth-Rose <gwyneth-rose.willis@ucdconnect.ie>
2026-02-18 23:19:15 +0000

    making polygons return to their original colour when unclicked

--- src/main/java/com/example/sequax40/app/Launcher.java
+++ src/main/java/com/example/sequax40/app/Launcher.java
@@ -1,6 +1,6 @@
 package com.example.sequax40.app;
 
-import javafx.application.Application;//changes
+import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
