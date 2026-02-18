efa72727dd408df88d850ff519422ac575ef3203
Gwyneth-Rose <gwyneth-rose.willis@ucdconnect.ie>
2026-02-18 22:41:04 +0000

    Board display: most of octagons + some rhombus

--- src/main/java/com/example/sequax40/controller/BoardController.java
+++ src/main/java/com/example/sequax40/controller/BoardController.java
@@ -2,23 +2,43 @@ package com.example.sequax40.controller;
 
 import com.example.sequax40.model.board.Board;
 import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
 
 public class BoardController {
+
+    private static final Color DEFAULT_COLOR = Color.web("#4d44ff");
+    private static final Color SELECTED_COLOR = Color.RED;
 
     private Board board;
 
     @FXML
     private GridPane boardGrid;
 
+
     public void initialize() {
         board = new Board(11, 11); // example 5x5 board
         // populate GridPane with buttons or tiles
     }
-
     @FXML
-    public void handleCellClick() {
-        // update board model and refresh UI
+    private void handleCellClick(MouseEvent event) {
+
+        Polygon clicked = (Polygon) event.getSource();
+
+        Boolean selected = (Boolean) clicked.getUserData();
+
+        if (selected == null || !selected) {
+            clicked.setFill(SELECTED_COLOR);
+            clicked.setUserData(true);
+        } else {
+            clicked.setFill(DEFAULT_COLOR);
+            clicked.setUserData(false);
+        }
     }
+
+
+
 }
