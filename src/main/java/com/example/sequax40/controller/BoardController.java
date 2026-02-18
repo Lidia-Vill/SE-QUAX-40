f29d25051780b6727528b8eedc25c7ca88e93731
Gwyneth-Rose <gwyneth-rose.willis@ucdconnect.ie>
2026-02-18 23:16:59 +0000

    making polygons return to their original colour when unclicked

--- src/main/java/com/example/sequax40/controller/BoardController.java
+++ src/main/java/com/example/sequax40/controller/BoardController.java
@@ -4,41 +4,42 @@ import com.example.sequax40.model.board.Board;
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
-import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 
 public class BoardController {
 
-    private static final Color DEFAULT_COLOR = Color.web("#4d44ff");
-    private static final Color SELECTED_COLOR = Color.RED;
+    private static final Color SELECTED_COLOR = Color.WHITE;
 
     private Board board;
 
     @FXML
     private GridPane boardGrid;
 
-
     public void initialize() {
         board = new Board(11, 11);
-        // populate GridPane with buttons or tiles
+
     }
+
     @FXML
     private void handleCellClick(MouseEvent event) {
 
         Polygon clicked = (Polygon) event.getSource();
 
-        Boolean selected = (Boolean) clicked.getUserData();
 
-        if (selected == null || !selected) {
+        Color originalColor = (Color) clicked.getUserData();
+
+
+        if (originalColor == null) {
+            clicked.setUserData(clicked.getFill());
             clicked.setFill(SELECTED_COLOR);
-            clicked.setUserData(true);
         } else {
-            clicked.setFill(DEFAULT_COLOR);
-            clicked.setUserData(false);
-        }//hey
+
+            if (clicked.getFill().equals(SELECTED_COLOR)) {
+                clicked.setFill(originalColor);
+            } else {
+                clicked.setFill(SELECTED_COLOR);
+            }
+        }
     }
-
-
-
 }
