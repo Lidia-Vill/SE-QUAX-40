4406b46b889fca2665b059297dcedba5558dcd55
annac-294 <anna.curry@ucdconnect.ie>
2026-03-06 14:18:43 +0000

    fixing

--- src/main/java/com/example/sequax40/controller/QuaxController.java
+++ src/main/java/com/example/sequax40/controller/QuaxController.java
@@ -4,20 +4,5 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class QuaxController {
-    /*
 
-    private GameState game;
-    
-    public void initialise() {
-        game = new GameState();
-    }
-
-    public void endTurn() {
-        game.nextPlayer();
-    }
-
-    public void restartGame() {
-        game.reset();
-    }
-    */
 }
